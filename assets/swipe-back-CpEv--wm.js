74ccad598f41ac0075579db3b2d9130ff19ae4d0
sascha-belski <sascha.belski@gmail.com>
2025-08-15 02:44:25 +0200

    Updates

--- assets/swipe-back-CpEv--wm.js
+++ assets/swipe-back-CpEv--wm.js
@@ -1,3 +1,3 @@
-import{i as h,c as D,d as M}from"./ionic-iR9r8-Ng.js";/*!
+import{i as h,c as D,d as M}from"./ionic-Bcn9dwnQ.js";/*!
  * (C) Ionic http://ionicframework.com - MIT License
  */const k=(n,m,g,p,X)=>{const c=n.ownerDocument.defaultView;let s=h(n);const w=t=>{const{startX:e}=t;return s?e>=c.innerWidth-50:e<=50},a=t=>s?-t.deltaX:t.deltaX,v=t=>s?-t.velocityX:t.velocityX;return D({el:n,gestureName:"goback-swipe",gesturePriority:101,threshold:10,canStart:t=>(s=h(n),w(t)&&m()),onStart:g,onMove:t=>{const e=a(t)/c.innerWidth;p(e)},onEnd:t=>{const o=a(t),e=c.innerWidth,r=o/e,i=v(t),y=e/2,l=i>=0&&(i>.2||o>y),d=(l?1-r:r)*e;let u=0;if(d>5){const f=d/Math.abs(i);u=Math.min(f,540)}X(l,r<=0?.01:M(0,r,.9999),u)}})};export{k as createSwipeBackGesture};
