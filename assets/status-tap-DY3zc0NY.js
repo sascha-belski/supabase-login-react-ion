731266aca3a2968e95082b0b56fa3526d1fa35a9
sascha-belski <sascha.belski@gmail.com>
2025-08-16 10:46:31 +0200

    Updates

--- assets/status-tap-DY3zc0NY.js
+++
@@ -1,3 +0,0 @@
-import{f as a,g as i,h as c,w as d,k as l}from"./ionic-Bcn9dwnQ.js";/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */const h=()=>{const e=window;e.addEventListener("statusTap",()=>{a(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=i(n);t&&new Promise(r=>c(t,r)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{h as startStatusTap};
