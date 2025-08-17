9faf99bb73914da096311936c5eda2c5c7569d0f
sascha-belski <sascha.belski@gmail.com>
2025-08-17 08:13:38 +0200

    Updates

--- assets/status-tap-BJY-DoaT.js
+++ assets/status-tap-BJY-DoaT.js
@@ -1,3 +1,3 @@
-import{Q as a,S as i,T as c,U as d,V as l}from"./ionic-BTAHEwko.js";/*!
+import{V as a,W as i,X as c,Y as d,Z as l}from"./ionic-DN6wCYrY.js";/*!
  * (C) Ionic http://ionicframework.com - MIT License
  */const m=()=>{const e=window;e.addEventListener("statusTap",()=>{a(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=i(n);t&&new Promise(r=>c(t,r)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{m as startStatusTap};
